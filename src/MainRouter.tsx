d8541dc0dd8b669f6bf043fe3add96950999f071
Kexort <kexort@gmail.com>
2024-08-12 14:46:22 +0200

    Use correct base for GH pages

--- src/MainRouter.tsx
+++ src/MainRouter.tsx
@@ -1,21 +1,21 @@
 import { ReactElement } from "react";
 import { Route, Routes } from "react-router-dom";
-import App from "../src/App";
-import Welcome1 from "../src/pages/Welcome1/Welcome1";
-import Welcome2 from "../src/pages/Welcome2/Welcome2";
-import Welcome3 from "../src/pages/Welcome3/Welcome3";
-import Welcome4 from "../src/pages/Welcome4/Welcome4";
-import OpenProfile from "../src/pages/OpenProfile/OpenProfile";
-import MainProfilePage from "../src/pages/MainProfilePage/MainProfilePage";
+import App from "./App";
+import Welcome1 from "./pages/Welcome1/Welcome1";
+import Welcome2 from "./pages/Welcome2/Welcome2";
+import Welcome3 from "./pages/Welcome3/Welcome3";
+import Welcome4 from "./pages/Welcome4/Welcome4";
+import OpenProfile from "./pages/OpenProfile/OpenProfile";
+import MainProfilePage from "./pages/MainProfilePage/MainProfilePage";
 
 export enum ROUTES {
-  APP = "/",
-  WELCOME1 = "/welcome1",
-  WELCOME2 = "/welcome2",
-  WELCOME3 = "/welcome3",
-  WELCOME4 = "/welcome4",
-  OPENPROFILE = "/openprofile",
-  MAINPROFILE = "/mainprofile",
+  APP = "/DevconAgora/",
+  WELCOME1 = "/DevconAgora/welcome1",
+  WELCOME2 = "/DevconAgora/welcome2",
+  WELCOME3 = "/DevconAgora/welcome3",
+  WELCOME4 = "/DevconAgora/welcome4",
+  OPENPROFILE = "/DevconAgora/openprofile",
+  MAINPROFILE = "/DevconAgora/mainprofile",
 }
 
 const MainRouter = (): ReactElement => {
--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import MainRouter from "../public/MainRouter";
+import MainRouter from "./MainRouter";
 import "./index.css";
 import { BrowserRouter } from "react-router-dom";
 
