8891e2b055ba3e3932f41ddb08c9acebeee429a0
Kexort <kexort@gmail.com>
2024-09-04 14:40:51 +0200

    Fix vite build

--- vite.config.ts
+++ vite.config.ts
@@ -6,8 +6,8 @@ export default defineConfig(({mode}) => {
   const env = loadEnv(mode, process.cwd(), '');
   return {
     define: {
-      'process.env.DEVCON6_SESSSIONS_HASH': JSON.stringify(env.DEVCON6_SESSSIONS_HASH) ?? '',
-      'process.env.BEE_API_URL': JSON.stringify(env.BEE_API_URL) ?? 'http://localhost:1633/'
+      'process.env.DEVCON6_SESSSIONS_HASH': JSON.stringify(env.DEVCON6_SESSSIONS_HASH) ?? JSON.stringify(''),
+      'process.env.BEE_API_URL': JSON.stringify(env.BEE_API_URL) ?? JSON.stringify('http://localhost:1633/')
     },
     plugins: [react()],
     base: '/DevconAgora/',
