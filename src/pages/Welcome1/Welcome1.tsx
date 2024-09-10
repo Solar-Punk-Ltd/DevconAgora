e185aa1dbef076c82b1edb737d537bb7ad7c07fe
Roland Seres <roland.seres90@gmail.com>
2024-09-10 16:12:30 +0200

    Welcome1 page mods

--- src/pages/Welcome1/Welcome1.tsx
+++ src/pages/Welcome1/Welcome1.tsx
@@ -1,10 +1,10 @@
 import React from "react";
 import { Link } from "react-router-dom";
-import welcome1img from "../../assets/welcome1img.png";
-import welcome1mask from "../../assets/welcome1mask.png";
+import welcome1img from "../../assets/ob_1.png";
 // import welcome1ColorEffectImg from "../../assets/welcome1-color-effect.png";
 import WelcomeButton from "../../components/WelcomeButton/WelcomeButton";
 import WelcomeProgressIndicator from "../../components/WelcomeProgressIndicator/WelcomeProgressIndicator";
+import WelcomeMask from "../../assets/welcome-glass-effect.png";
 
 const Welcome1: React.FC = () => {
   return (
@@ -20,43 +20,50 @@ const Welcome1: React.FC = () => {
         <div className="welcome-page__content">
           {/* <div className="welcome1-page__content__color-effect"></div> */}
 
-          <div
-            className="welcome-page__content__image"
-            style={{ position: "relative" }}
-          >
-            <div
-              style={{ position: "absolute", bottom: "0px", display: "flex" }}
-            >
-              <img src={welcome1mask} alt="" width="100%" />
+          <div className="welcome-page__content__image">
+            <img src={welcome1img} alt="" height="150%" />
+            <div className="welcome-page__effect">
+              <img
+                src={WelcomeMask}
+                alt=""
+                style={{
+                  position: "absolute",
+                  bottom: "-62px",
+                  left: "0px",
+                  width: "100%",
+                  height: "65%",
+                  zIndex: "-1",
+                }}
+              />
             </div>
-            <div></div>
-            <img src={welcome1img} alt="" width="293px" />
-          </div>
-          <div className="welcome-page__content__text">
-            Be Part of the Ethereum Community Contribute of the Growth of the
-            Ecosystem.
           </div>
         </div>
         <div className="welcome-page__bottom">
-          <WelcomeProgressIndicator active={0} />
-          <div className="welcome-page-buttons">
-            <Link to="/welcome2" style={{ flex: "1 1 0" }}>
-              <WelcomeButton version="filled">Next</WelcomeButton>
-            </Link>
+          <div className="welcome-page__content__text">
+            <div>
+              Be Part of the Ethereum Community Contribute of the Growth of the
+              Ecosystem.
+            </div>
           </div>
-          <div className="welcome-page__bottom-text">
-            I know everything{" "}
-            <Link to="/openprofile">
-              <span
-                style={{
-                  textDecoration: "underline",
-                  fontWeight: "bold",
-                  color: "#8C72AE",
-                }}
-              >
-                SKIP
-              </span>
+          <div className="welcome-page__bottom-bottom">
+            <WelcomeProgressIndicator active={0} />
+            <Link to="/welcome2">
+              <WelcomeButton>Next</WelcomeButton>
             </Link>
+            <div className="welcome-page__bottom-text">
+              I know everything{" "}
+              <Link to="/openprofile">
+                <span
+                  style={{
+                    textDecoration: "underline",
+                    fontWeight: "bold",
+                    color: "#8C72AE",
+                  }}
+                >
+                  SKIP
+                </span>
+              </Link>
+            </div>
           </div>
         </div>
       </div>
