8f8445b5baa3eed8cd551b7a2210a0376f8b4201
Peter Ott <peter.ott@solarpunk.buzz>
2024-09-10 17:08:05 +0200

    connect with external repos

--- src/pages/MainProfilePage/MainProfilePage.tsx
+++ src/pages/MainProfilePage/MainProfilePage.tsx
@@ -10,7 +10,7 @@ import { Swarm } from "libswarm";
 import { ADDRESS_HEX_LENGTH } from "../../utils/constants";
 import { ethers, ZeroHash } from "ethers";
 import { Bee, Signer, Topic, Utils } from "@ethersphere/bee-js";
-import { SwarmCommentSystem, SwarmCommentSystemProps } from "../../components/Comment/SwarmCommentSystem";
+import { SwarmCommentSystem, SwarmCommentSystemProps } from '@ethersphere/comment-system-ui';
 
 const TEMP_BEE_API_URL = "http://161.97.125.121:1733/";
 const TEMP_DEVCON6_SESSSIONS_HASH = "4e4d8fa5cb134fef91e29c367f5aaf448d8133f91a58c08408e06c94cea8dd8b"
