674ac6b81ab8fa39fbd5ec53582951c61eb38eb0
Peter Ott <peter.ott@solarpunk.buzz>
2024-09-11 16:08:05 +0200

    removed vite config changes

--- vite.config.ts
+++ vite.config.ts
@@ -1,24 +1,15 @@
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv } from 'vite'
 import react from '@vitejs/plugin-react'
-import path from 'path'
 
 // https://vitejs.dev/config/
-export default defineConfig(() => {
+export default defineConfig(({mode}) => {
+  const env = loadEnv(mode, process.cwd(), '');
   return {
-
+    define: {
+      'process.env.DEVCON6_SESSSIONS_HASH': JSON.stringify(env.DEVCON6_SESSSIONS_HASH) ?? JSON.stringify(''),
+      'process.env.BEE_API_URL': JSON.stringify(env.BEE_API_URL) ?? JSON.stringify('http://localhost:1633/')
+    },
     plugins: [react()],
     base: '/DevconAgora/',
-    build: {
-      sourcemap: true,
-      rollupOptions: {
-        external: ['react', 'react-dom'],
-      },
-    },
-    resolve: {
-      alias: {
-        react: path.resolve('./node_modules/react'),
-        'react-dom': path.resolve('./node_modules/react-dom')
-      }
-    }
   }
 })
