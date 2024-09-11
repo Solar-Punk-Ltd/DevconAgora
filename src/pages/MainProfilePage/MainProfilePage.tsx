d98988d10f34dc92aee8af1d5b0b0755bc822db5
Peter Ott <peter.ott@solarpunk.buzz>
2024-09-11 16:13:50 +0200

    modifies MainProfilePage

--- src/pages/MainProfilePage/MainProfilePage.tsx
+++ src/pages/MainProfilePage/MainProfilePage.tsx
@@ -1,23 +1,29 @@
-import BlogPostBox from "../../components/BlogPostBox/BlogPostBox";
+import React from "react";
 import DevConMainBox from "../../components/DevConMainBox/DevConMainBox";
-import RecentBox from "../../components/RecentBox/RecentBox";
-import UpcomingTalkBox from "../../components/UpcomingTalkBox/UpcomingTalkBox";
-import Header from "../Header/Header";
+import RecentTalks from "../../components/RecentTalks/RecentTalks";
+// import UpcomingTalkBox from "../../components/UpcomingTalkBox/UpcomingTalkBox";
 import "./MainProfilePage.scss";
 import { Session } from "../../types/session";
-import React, { useEffect, useState } from "react";
+import { useEffect, useState } from "react";
 import { Swarm } from "libswarm";
 import { ADDRESS_HEX_LENGTH } from "../../utils/constants";
-import { ethers, ZeroHash } from "ethers";
-import { Bee, Signer, Topic, Utils } from "@ethersphere/bee-js";
-import { SwarmCommentSystem } from '@ethersphere/comment-system-ui';
-
-const TEMP_BEE_API_URL = "http://161.97.125.121:1733/";
-const TEMP_DEVCON6_SESSSIONS_HASH = "4e4d8fa5cb134fef91e29c367f5aaf448d8133f91a58c08408e06c94cea8dd8b"
+import RecentRooms from "../../components/RecentRooms/RecentRooms";
+import NavigationFooter from "../../components/NavigationFooter/NavigationFooter";
+import Header from "../../components/Header/Header";
+import { ethers } from "ethers";
+import { Signer, Utils } from "@ethersphere/bee-js";
+import { SwarmCommentSystem } from "@ethersphere/comment-system-ui";
 
 
 const MainProfilePage: React.FC = () => {
   const [sessions, setSessions] = useState<Session[]>([]);
+  const [isBeeRunning, setBeeRunning] = useState(false);
+  const [postageStamp, setPostageStamp] = useState<string>("");
+
+  const swarm = new Swarm({
+    beeApi: process.env.BEE_API_URL,
+    postageBatchId: "todo dummy",
+  });
 
   // Create Wallet - this will be created outside the component
   let wallet: ethers.Wallet | null;
@@ -37,8 +43,29 @@ const MainProfilePage: React.FC = () => {
     },
   };
 
-  const bee = new Bee(TEMP_BEE_API_URL);
-  const swarm = new Swarm({ beeApi: TEMP_BEE_API_URL });
+  async function checkBee() {
+    fetch(process.env.BEE_API_URL + "addresses")
+      .then(async () => {
+        if (!isBeeRunning) {
+          setBeeRunning(true);
+          console.log("Bee is running");
+        }
+        if (postageStamp.length === 0) {
+          const stamp = await swarm.getUsableStamp();
+          if (stamp === null) {
+            console.log("No usable postage stamp found");
+          } else {
+            setPostageStamp(stamp);
+            console.log("Usable postage stamp found: " + stamp);
+          }
+        }
+      })
+      .catch(() => {
+        setBeeRunning(false);
+        setPostageStamp("");
+        console.log("Bee stopped running");
+      });
+  }
 
   async function getSessions(hash: string) {
     if (hash.length !== ADDRESS_HEX_LENGTH) {
@@ -50,20 +77,19 @@ const MainProfilePage: React.FC = () => {
       const data = JSON.parse(
         (await swarm.downloadRawData(hash, "application/json")).utf8
       );
-      //console.log("data: ", data)
       const s: Session[] = data.data.items;
-      //console.log("s: ", s)
       setSessions(sessions.concat(s));
     } catch (e) {
-      //console.log("talk " + hash + " download/cast error", e);
+      console.log("talk " + hash + " download/cast error", e);
     }
   }
 
   useEffect(() => {
+    checkBee();
     if (sessions.length === 0) {
-      getSessions(TEMP_DEVCON6_SESSSIONS_HASH);
+      getSessions(process.env.DEVCON6_SESSSIONS_HASH || "");
     }
-  }, [sessions]);
+  });
 
   return (
     <div
@@ -71,31 +97,38 @@ const MainProfilePage: React.FC = () => {
         height: "100vh",
         boxSizing: "border-box",
         backgroundColor: "#f5f5f5",
-        padding: "20px",
         display: "flex",
         flexDirection: "column",
-        justifyContent: "space-between",
       }}
     >
       <Header name="Agora"></Header>
-      <DevConMainBox />
-      <RecentBox />
-      
+      <div style={{ padding: "15px" }}>
+        <DevConMainBox
+          title="Devcon chatroom"
+          content="Share your tought, chat with anybody without moderation and collect the reward."
+          showActiveVisitors={true}
+          activeVisitors={110}
+          furtherInfo="Tell us how are you!"
+        />
+        <RecentTalks />
+        <RecentRooms />
+        {/* <UpcomingTalkBox sessions={sessions} /> */}
+
       {/** 
        * Talks will only have this. Room should already exist when the application is launched
        * A human-readable topic needs to exist, and one master private key is enough, that is burnt in into the application,
        * rooms will be distinguished by topic
        */}
-      <SwarmCommentSystem 
-        stamp={"9a478f8f6ac0f2882654a1b81680ee021fbc8bcb5280d5342326d1d766b085fa"}
+      <SwarmCommentSystem
+        stamp={postageStamp}
         topic={"bagoytopic"}
         privateKey={wallet.privateKey}
-        beeApiUrl={TEMP_BEE_API_URL}
+        beeApiUrl={process.env.BEE_API_URL}
         signer={signer}
       />
 
-      <UpcomingTalkBox sessions={sessions} />
-      <BlogPostBox />
+      </div>
+      <NavigationFooter />
     </div>
   );
 };
