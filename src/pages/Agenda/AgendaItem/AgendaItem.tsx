d79cf4ca7bfd2bdfa41e0e7321305b776ab31ae5
rolandlor <33499567+rolandlor@users.noreply.github.com>
2024-09-16 10:01:32 +0200

    Agenda UI modifications (#9)

--- src/pages/Agenda/AgendaItem/AgendaItem.tsx
+++ src/pages/Agenda/AgendaItem/AgendaItem.tsx
@@ -1,47 +1,35 @@
 import React from "react";
 import "./AgendaItem.scss";
-import EmptyHeartIcon from "../../../assets/empty-heart.png";
+import CategoryIndicator from "../../../components/CategoryIndicator/CategoryIndicator";
+import HeartIcon from "../../../components/icons/HeartIcon/HeartIcon";
 
 interface AgendaItemProps {
   name?: string;
   startDate?: string;
   endDate?: string;
+  hearted?: boolean;
 }
 
 const AgendaItem: React.FC<AgendaItemProps> = ({
   name,
   startDate,
   endDate,
+  hearted,
 }) => {
   return (
-    <div
-      style={{
-        display: "flex",
-        justifyContent: "center",
-        height: "64px",
-        alignItems: "center",
-        borderBottom: "1px solid #C8C8CA",
-      }}
-    >
-      <div
-        style={{
-          minWidth: "56px",
-          display: "flex",
-          flexDirection: "column",
-          alignItems: "end",
-          paddingRight: "9px",
-          paddingLeft: "9px",
-          borderRight: "1px solid #C8C8CA",
-        }}
-      >
+    <div className="agenda-item">
+      <div className={"agenda-item__time"}>
         <div style={{ fontSize: "12px", fontWeight: "400" }}>{startDate}</div>
         <div style={{ fontSize: "12px", fontWeight: "400" }}>{endDate}</div>
       </div>
-      <div style={{ fontSize: "12px", fontWeight: "700", paddingLeft: "9px" }}>
-        {name}
+      <div className="agenda-item__content">
+        <div className="agenda-item__content__name">{name}</div>
+        <div className="agenda-item__content__category">
+          <CategoryIndicator name="Layer 2s" />
+        </div>
       </div>
       <div style={{ width: "24px", paddingRight: "9px" }}>
-        <img src={EmptyHeartIcon} alt="" width="24px" height="24px" />
+        <HeartIcon empty={!hearted} />
       </div>
     </div>
   );
