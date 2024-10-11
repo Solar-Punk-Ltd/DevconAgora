694644b23006cf0156363e650e69f3adee7935f1
Peter Ott <ottpeter@tuta.io>
2024-10-11 12:50:27 +0200

    Adds RoomWithUserCounts type

--- src/types/room.ts
+++ src/types/room.ts
@@ -4,3 +4,10 @@ export interface Room {
   info: string;
   capacity: string;
 }
+
+export interface RoomWithUserCounts {
+  url: string,
+  gateway: string,
+  topic: string,
+  userCount?: number
+}
