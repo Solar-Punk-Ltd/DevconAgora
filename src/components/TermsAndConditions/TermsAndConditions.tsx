1ac6435f05e8ce32cd2b3d8739f40fb401c8722c
rolandlor <33499567+rolandlor@users.noreply.github.com>
2024-10-16 10:28:40 +0200

    UI-Bug-Fixing-4 (#49)
    Co-authored-by: Bálint Ujvári <balint.ujvari@solarpunk.buzz>

--- src/components/TermsAndConditions/TermsAndConditions.tsx
+++ src/components/TermsAndConditions/TermsAndConditions.tsx
@@ -2,11 +2,15 @@ import React from "react";
 import "./TermsAndConditions.scss";
 import {
   DISCLAIMER_OF_LIABILITY_HEADER,
-  DISCLAIMER_OF_LIABILITY,
+  DISCLAIMER_OF_LIABILITY_TEXT,
   PERSONAL_DATA_HEADER,
-  PERSONAL_DATA,
+  PERSONAL_DATA_TEXT,
   DATA_STORAGE_HEADER,
-  DATA_STORAGE,
+  DATA_STORAGE_TEXT,
+  CONTENT_FILTER_HEADER,
+  CONTENT_FILTER_TEXT,
+  ACCEPTANCE_OF_TERMS_HEADER,
+  ACCEPTANCE_OF_TERMS_TEXT,
 } from "../../utils/constants";
 import CheckBoxIcon from "../icons/CheckBoxIcon/CheckBoxIcon";
 import { useGlobalState } from "../../GlobalStateContext";
@@ -32,16 +36,26 @@ const TermsAndConditions: React.FC<TermsAndConditionsProps> = ({
         {DISCLAIMER_OF_LIABILITY_HEADER}
       </div>
       <div className="terms-and-conditions__text">
-        {DISCLAIMER_OF_LIABILITY}
+        {DISCLAIMER_OF_LIABILITY_TEXT}
       </div>
       <div className="terms-and-conditions__text__header">
         {PERSONAL_DATA_HEADER}
       </div>
-      <div className="terms-and-conditions__text">{PERSONAL_DATA}</div>
+      <div className="terms-and-conditions__text">{PERSONAL_DATA_TEXT}</div>
       <div className="terms-and-conditions__text__header">
         {DATA_STORAGE_HEADER}
       </div>
-      <div className="terms-and-conditions__text">{DATA_STORAGE}</div>
+      <div className="terms-and-conditions__text">{DATA_STORAGE_TEXT}</div>
+      <div className="terms-and-conditions__text__header">
+        {CONTENT_FILTER_HEADER}
+      </div>
+      <div className="terms-and-conditions__text">{CONTENT_FILTER_TEXT}</div>
+      <div className="terms-and-conditions__text__header">
+        {ACCEPTANCE_OF_TERMS_HEADER}
+      </div>
+      <div className="terms-and-conditions__text">
+        {ACCEPTANCE_OF_TERMS_TEXT}
+      </div>
 
       {contentFilterCheckBox ? (
         <div className="terms-and-conditions__checkbox-setting">
