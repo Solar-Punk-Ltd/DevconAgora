8ecdc786a9bbdc9bbf41549a07cdd034499bfebe
Roland Seres <roland.seres90@gmail.com>
2024-10-24 16:27:25 +0200

    UI-talks-Refactor

--- src/components/TalkItem/TalkItem.tsx
+++ src/components/TalkItem/TalkItem.tsx
@@ -88,25 +88,21 @@ const TalkItem: React.FC<TalkItemProps> = ({ session }) => {
 
   return (
     <>
-      <div className="talk-item">
-        {session && (
-          <AgendaItem
-            key={session.id}
-            id={session.id}
-            title={session.title}
-            startDate={dateToTime(session.slot_start)}
-            endDate={dateToTime(session.slot_end)}
-            category={session.track}
-            roomId={session.slot_roomId}
-            liked={session.liked}
-            paddingRight={"16px"}
-            stage={STAGES_MAP.get(
-              session.slot_roomId ? session.slot_roomId : ""
-            )}
-            commentVersion={true}
-          />
-        )}
-      </div>
+      {session && (
+        <AgendaItem
+          key={session.id}
+          id={session.id}
+          title={session.title}
+          startDate={dateToTime(session.slot_start)}
+          endDate={dateToTime(session.slot_end)}
+          category={session.track}
+          roomId={session.slot_roomId}
+          liked={session.liked}
+          paddingRight={"16px"}
+          stage={STAGES_MAP.get(session.slot_roomId ? session.slot_roomId : "")}
+          commentVersion={true}
+        />
+      )}
       {/* // either use a local stamp from the env or a dummy can be sent to the
       gateway */}
       {!loading && (
