ddc5e270d190d31dbfbbcd2b48cfe1a6bcb310a6
Peter Ott <ottpeter@tuta.io>
2024-11-06 17:09:16 +0100

    adds safeguard for like as well

--- src/pages/Chat/Chat.tsx
+++ src/pages/Chat/Chat.tsx
@@ -173,7 +173,11 @@ const Chat: React.FC<ChatProps> = ({
         const likedIndex = threadCapableMessages.findIndex(
           (msg) => msg.messageId === likeObj.like
         );
-        threadCapableMessages[likedIndex].likeTable[address] = true;
+        if (likedIndex === -1) {
+          console.warn("This thread does not exist (will not add like)");
+        } else {
+          threadCapableMessages[likedIndex].likeTable[address] = true;
+        }
       } else {
         threadCapableMessages.push({
           username: data[i].username,
