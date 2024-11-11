58f3825b85a1e9507361d399f69fb4a3407bf551
Levente Kiss <levente.kiss@solarpunk.bzz>
2024-11-11 14:22:15 +0700

    refactor: moving components to their right places

---
+++ src/pages/Intro/Intro.tsx
@@ -0,0 +1,58 @@
+import { useEffect } from "react";
+import { useNavigate } from "react-router-dom";
+import "./intro.scss";
+import "../src/styles/global.scss";
+import { ROUTES } from "../../utils/constants";
+
+import backgroundVideo from "./assets/opening.mp4";
+import bySolarPunk from "./assets/by-solar-punk.png";
+import videoGlassEffect from "./assets/video-glass-effect.png";
+import dc7Logo from "./assets/dc7.png";
+import { TEXTS } from "../../../textConstants";
+import { isUserRegistered } from "../../utils/helpers";
+
+function Intro() {
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      // Check if user is already registered
+      if (isUserRegistered()) {
+        navigate(ROUTES.HOME);
+      } else {
+        navigate(ROUTES.WELCOME1);
+      }
+    }, 5000);
+
+    return () => {
+      clearTimeout(timer);
+    };
+  }, [navigate]);
+  return (
+    <>
+      <div className="opening-page">
+        <video autoPlay muted loop className="opening-page__video-background">
+          <source src={backgroundVideo} type="video/mp4" />
+        </video>
+        <div>
+          <img
+            src={videoGlassEffect}
+            className="opening-page__video-glass-effect"
+          />
+        </div>
+        <div style={{}} className="opening-page__text-layer">
+          <div className="opening-page__header">
+            <div>{TEXTS.APP_NAME}</div>
+
+            <img src={bySolarPunk} alt="" height="30px" width="104px" />
+          </div>
+          <div className="opening-page__footer">
+            <img src={dc7Logo} alt="" height="57px" width="160px" />
+          </div>
+        </div>
+      </div>
+    </>
+  );
+}
+
+export default Intro;
