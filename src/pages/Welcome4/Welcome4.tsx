216eb2480364d07a127d669a6a95d6c81d717c55
Andrei Mitrea <andrei.mitrea.hq@gmail.com>
2025-09-02 17:40:52 +0300

    feat: Updated welcome screens layout

--- src/pages/Welcome4/Welcome4.tsx
+++ src/pages/Welcome4/Welcome4.tsx
@@ -1,7 +1,6 @@
 import React from "react";
 import { Link } from "react-router-dom";
 
-import WelcomeMask from "../../assets/welcome-glass-effect.png";
 import welcome4img from "../../assets/welcome4img.png";
 import WelcomeButton from "../../components/WelcomeButton/WelcomeButton";
 import WelcomeProgressIndicator from "../../components/WelcomeProgressIndicator/WelcomeProgressIndicator";
@@ -17,9 +16,6 @@ const Welcome1: React.FC = () => {
         <div className="welcome-page__content">
           <div className="welcome-page__content__image">
             <img src={welcome4img} alt="" className="welcome-page__main-image" />
-            <div className="welcome-page__effect">
-              <img src={WelcomeMask} alt="" className="welcome-page__effect-mask" />
-            </div>
           </div>
         </div>
         <div className="welcome-page__bottom">
